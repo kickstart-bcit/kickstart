7fd6d519c1cc53d60d4f08bc20177cc0019f584f
viviannguyen <viviannguyen.bee@gmail.com>
2019-03-29 09:23:54 -0700

    staff rewards styling

--- connectors/rewardsConnector.js
+++ connectors/rewardsConnector.js
@@ -208,8 +208,8 @@ const redeemRewards = (sid, rid) => {
 const renderReedemableRewards = (rewards, sid) => {
   return rewards.map(
     item =>
-      `<div><p><span>Title: </span>${item.rewards_title}<span>Points: </span>${item.rewards_points
-      }<button onclick="redeem(${item.rewards_id},'${sid}')">redeem</button></p></div>`
+      `<div><p><button onclick="redeem(${item.rewards_id},'${sid}')">redeem</button>&nbsp<span>Title: </span>${item.rewards_title}<span> &nbsp | &nbsp Points: </span>${item.rewards_points
+      } </p></div>`
   ).join("").replace(/\s\s+/g, " ");
 };
 
